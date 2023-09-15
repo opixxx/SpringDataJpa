5ccf7c3f727e137be2b8b7f2a1677112bd0e0384
opixxx <opix0306@naver.com>
2023-09-15 14:28:04 +0900

    @Query 를 이용하요 리포지토리 메소드에 쿼리 정의하기

--- src/main/java/spring/dataJPA/repository/MemberRepository.java
+++ src/main/java/spring/dataJPA/repository/MemberRepository.java
@@ -1,10 +1,15 @@
 package spring.dataJPA.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import spring.dataJPA.entitiy.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
